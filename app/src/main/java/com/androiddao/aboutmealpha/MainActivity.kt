1a3c8744838529a5de6690909a47729adc730f09
AndroidDAO <rigenskylouiszgmail.com>
2023-03-18 19:05:42 +0300

    Initial commit

---
+++ app/src/main/java/com/androiddao/aboutmealpha/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.androiddao.aboutmealpha
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)//content of this activity is to be shown in the layout
+    }
+}
